046096a70fe3cf2c2b5157c6084bf5f460dba79c
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2025-10-23 14:35:54 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Header from "./components/Header.jsx";
 const App = () => {
-  return <div>
-    
-
-
-  </div>;
+  return (
+    <div>
+      <Header />
+      <h1>main</h1>
+    </div>
+  );
 };
 
 export default App;

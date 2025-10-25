b11563e0602814578bbf2282ed3841a9af62fd63
Ziyodilla11 <ziyodillanasriddinov014@gmail.com>
2025-10-25 14:16:00 +0500

    cetr

---
+++ src/components/Zfoot.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+const Header = () => {
+  return (
+<div className="Zfooter">
+      <div className="Zcontainer">
+<hr />
+      <p>* This information is estimated based on consumers who were approved for an auto refinance loan through Caribou on or after 1/1/2022, had an existing auto loan on their credit report, and accepted their final terms. As of 9/27/2022, borrowers who refinance save an average of $111.16 per month. Refinance savings may result from a lower interest rate, longer term, or both. There is no guarantee of savings. Your actual savings, if any, may vary based on interest rates, the repayment term, the amount financed, and other factors.</p>
+      <p>+ To check the refinance rates and terms you qualify for, we conduct a soft credit pull that will not affect your credit score. However, if you choose a loan product and continue your application, we or one of our lending partners will request your full credit report from one or more consumer reporting agencies, which is considered a hard credit pull and may affect your credit.</p>
+      <p>++ Social security number is required should you choose to move forward in the loan application process.</p>
+      <p>** APR is the Annual Percentage Rate. Your actual APR may be different. Your APR is based on multiple factors including your credit profile and the loan to value of the vehicle. APR ranges from 2.83% to 36.00% and is determined at the time of application. Lowest APR is based on loan amount of $45,000, 36 month term, and is available to borrowers with excellent credit and only in certain states. Conditions apply. Advertised rates and fees are valid as of 11/1/2022 and are subject to change without notice. Insurance savings will not result from lower APR.</p>
+      <p>Terms and Conditions apply. Caribou reserves the right to modify or discontinue products and benefits at any time without notice. Participating lenders, rates and terms are also subject to change at any time without notice. The information you provide to us is an inquiry to determine whether our lenders can make you a loan offer. If any of our lending partners has an available loan offer for you, you will be invited to submit a loan application to the lender for its review. Not all borrowers receive the lowest rate. Lowest rates are reserved for the highest qualified borrowers. We do not guarantee that you will receive any loan offers or that your loan application will be approved. If approved, your actual rate will depend on a variety of factors, including term of loan, a responsible financial history, income and other factors. Offers not available in MA, MD, MS, NE, NV, WI, WV.</p>
+      <p>Insurance products offered through Bindable and Caribou Insurance Services, LLC. Caribou is working with Bindable who owns MyLifeProtected and MassDrive Insurance Group, LLC, the licensed agent for all products.</p>
+    <hr />
+    <p>©2023 Caribou Financial, Inc. d/b/a Caribou. All rights reserved.</p>
+             <div className="Ztexts2">
+         <p>Electronic Communications Consent</p>
+         <p>Privacy Policy</p>
+         <p>Your California Privacy Rights</p>
+         <p>Terms of Use</p>
+         <p>Disclosures</p>
+         <p>NMLS Consumer Access</p>
+             </div>
+       </div>
+</div>
+  );
+};
+
+export default Header;
